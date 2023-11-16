4e98f619e3325a98e70da70974500c7951dc4a82
Nekute <gtab4a@gmail.com>
2023-11-16 23:56:20 +0100

    Inversion des icones de changement de thème

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,34 +57,34 @@ class Header extends Component {
                 width={90}
                 height={40}
                 uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
+                    <span
+                        className="iconify"
+                        data-icon="noto-v1:sun-with-face"
+                        data-inline="false"
+                        style={{
+                            display: "block",
+                            height: "100%",
+                            fontSize: 25,
+                            textAlign: "end",
+                            marginLeft: "15px",
+                            color: "#353239",
+                        }}
+                    ></span>
                 }
                 checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
+                    <span
+                        className="iconify"
+                        data-icon="twemoji:owl"
+                        data-inline="false"
+                        style={{
+                            display: "block",
+                            height: "100%",
+                            fontSize: 25,
+                            textAlign: "end",
+                            marginLeft: "15px",
+                            color: "#353239",
+                        }}
+                    ></span>
                 }
                 id="icon-switch"
               />
