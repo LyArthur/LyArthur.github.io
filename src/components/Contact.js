5f3db2e3e90e33d16ff8963290e0f7a01011cf4c
Arthur LY <gtab4A@gmail.com>
2023-11-24 08:48:54 +0100

    début du formulaire de contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,26 @@
 import React, {Component} from "react";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: '', mail: '', object: '', message: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const name = target.name;
+        console.log(event.target);
+        this.setState({[name]: target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.name);
+        event.preventDefault();
+    }
+
     render() {
         if (this.props.resumeContact && this.props.resumeBasicInfo) {
             var sectionName = this.props.resumeBasicInfo.section_name.contact;
@@ -9,10 +29,10 @@ class Contact extends Component {
                     <li className="list-inline-item mx-3" key={i}>
             <span>
                 <a className="social-tile"
-                href={contact.url}
-                target={"_blank"}>
+                   href={contact.url}
+                   target={"_blank"}>
               <div className="text-center contact-tile">
-                <i className={contact.class} style={{ fontSize: "300%" }}></i>
+                <i className={contact.class} style={{fontSize: "300%"}}></i>
               </div>
                     </a>
             </span>
@@ -30,6 +50,24 @@ class Contact extends Component {
                 </div>
                 <div className="col-md-12 text-center">
                     <ul className="list-inline mx-auto skill-icon">{contact}</ul>
+                    <form onSubmit={this.handleSubmit}>
+                        <label for={"name"}>Name:</label>
+                        <input type="text" name={"name"} value={this.state.name} onChange={this.handleChange}/>
+                        <label>
+                            Name:
+                            <input type="text" name={"mail"} value={this.state.mail} onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Name:
+                            <input type="text" name={"object"} value={this.state.object} onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Name:
+                            <input type="text" name={"message"} value={this.state.message}
+                                   onChange={this.handleChange}/>
+                        </label>
+                        <input type="submit" value="Submit"/>
+                    </form>
                 </div>
             </div>
         </section>);
