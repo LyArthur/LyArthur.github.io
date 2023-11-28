05a5528b4cd3a72da725763415b8c4025012389a
Nekute <gtab4a@gmail.com>
2023-11-28 23:45:34 +0100

    installation du front de contact

--- src/functions/mailContact.js
+++ src/functions/mailContact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
@@ -6,33 +6,57 @@ export const ContactUs = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
+        const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-        emailjs.sendForm('service_ihkfivr', 'template_e07x3hf', form.current, 'lsN6WThGlV0hYHzTI')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        if (emailRegexp.test(form.current.email.value)){
+            emailjs.sendForm('service_ihkfivr', 'template_e07x3hf', form.current, 'lsN6WThGlV0hYHzTI')
+                .then((result) => {
+                    alert("Le message a bien été envoyé !");
+                }, (error) => {
+                    alert("il y a eu un problème");
+                });
+        } else {
+            alert("il y a eu un problème");
+        }
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label htmlFor={"name"}>Name:</label>
-            <input type="text" name={"name"} />
-            <label>
-                Name:
-                <input type="text" name={"mail"} />
-            </label>
-            <label>
-                Name:
-                <input type="text" name={"object"}/>
-            </label>
-            <label>
-                Name:
-                <input type="text" name={"message"}
-                />
-            </label>
-            <input type="submit" value="Submit"/>
-        </form>
+        <div className="contact-container">
+            <form ref={form} onSubmit={sendEmail} className="contact-form">
+                <div className="input_container name">
+                    <label className="input_label" htmlFor="name_field">{localStorage['language']==="fr" ? "Nom" : "Name"}</label>
+                    <input
+                        placeholder="Nom"
+                        title="Inpit title"
+                        name="name"
+                        type="text"
+                        className="input_field"
+                        id="name_field"
+                        required={true}/>
+                </div>
+                <div className="input_container email">
+                    <label className="input_label" htmlFor="email_field">Email</label>
+                    <input
+                        placeholder="nom@mail.com"
+                        title="Inpit title"
+                        name="email"
+                        type="text"
+                        className="input_field"
+                        id="email_field"
+                        required={true}/>
+                </div>
+                <div className="input_container message">
+                    <label className="input_label" htmlFor="message_field">Message</label>
+                    <textarea
+                        placeholder="Message.."
+                        title="Inpit title"
+                        name="message"
+                        className="input_field"
+                        id="message_field"
+                        required={true}/>
+                </div>
+                <input className="submit" type="submit" value="Submit"/>
+            </form>
+        </div>
     );
 };
