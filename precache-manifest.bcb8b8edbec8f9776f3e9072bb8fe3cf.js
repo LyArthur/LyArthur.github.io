33972d5a415a13eb4aa19d28ac02b2dec0b7a737
Nekute <gtab4a@gmail.com>
2023-11-28 23:59:29 +0100

    Updates

---
+++ precache-manifest.bcb8b8edbec8f9776f3e9072bb8fe3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3590eb799b931ff1d1b015ac4c661db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63506ca2db6a4675bed9",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "9d4579c4f6a092ad08c9",
+    "url": "/static/css/main.e544c7cd.chunk.css"
+  },
+  {
+    "revision": "63506ca2db6a4675bed9",
+    "url": "/static/js/2.1848f15c.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/static/js/2.1848f15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4579c4f6a092ad08c9",
+    "url": "/static/js/main.7fe425f9.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
